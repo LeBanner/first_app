f9cfc862c89007662dae20cab48a5f0dad67de8a
LeBanner <lecca.liebe@gmail.com>
2021-07-26 12:46:36 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+  def index  # indexアクションを定義した
+    @posts = Post.all  # すべてのレコードを@postsに代入
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
+
+
+class PostsController < ApplicationController
+  def index
+  end
+end
